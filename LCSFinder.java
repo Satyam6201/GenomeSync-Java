fdd510252e52fa9a49f3dbfc517bd7c65f431454
satyam kumar mishra <satyamkmishraa@gmail.com>
2025-07-03 14:29:14 +0530

    GenomeSync-Java

--- LCSFinder.java
+++ LCSFinder.java
@@ -1,3 +1,25 @@
 public class LCSFinder {
-    
+    public static String findLCS(String s1, String s2) {
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        // Reconstruct LCS
+        StringBuilder lcs = new StringBuilder();
+        int i = s1.length(), j = s2.length();
+        while (i > 0 && j > 0) {
+            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                lcs.insert(0, s1.charAt(i - 1));
+                i--; j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) i--;
+            else j--;
+        }
+        return lcs.toString();
+    }
 }
