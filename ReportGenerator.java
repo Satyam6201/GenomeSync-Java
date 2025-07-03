a701dbfa9eb2335156ea1296b015eabbd58eabcf
satyam kumar mishra <satyamkmishraa@gmail.com>
2025-07-03 14:30:26 +0530

    GenomeSync-Java

--- ReportGenerator.java
+++ ReportGenerator.java
@@ -1,3 +1,13 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ReportGenerator {
-    
+    public static void saveReport(String content, String path) {
+        try (FileWriter writer = new FileWriter(path)) {
+            writer.write(content);
+            System.out.println("Report saved to " + path);
+        } catch (IOException e) {
+            System.out.println("Failed to save report.");
+        }
+    }
 }
