852fe206a8fa759c0bc605fc781c099b124e706f
satyam kumar mishra <satyamkmishraa@gmail.com>
2025-07-03 14:31:08 +0530

    GenomeSync-Java

--- TimerUtil.java
+++ TimerUtil.java
@@ -1,3 +1,11 @@
 public class TimerUtil {
-    
+    private long start;
+
+    public void start() {
+        start = System.currentTimeMillis();
+    }
+
+    public long stop() {
+        return System.currentTimeMillis() - start;
+    }
 }
