4212f54a6dd089dd5ecf112296c67945901bc7ac
satyam kumar mishra <satyamkmishraa@gmail.com>
2025-07-03 14:28:23 +0530

    GenomeSync-Java

--- DNASequenceMatcher.java
+++ DNASequenceMatcher.java
@@ -1,10 +1,18 @@
 public class DNASequenceMatcher {
     public static boolean isValidDNA(String sequence) {
-        return sequence.matches("[ATGC]+");
+        return sequence != null && sequence.matches("[ATGC]+");
     }
 
     public static double similarityPercentage(String s1, String s2, int distance) {
         int maxLength = Math.max(s1.length(), s2.length());
-        return 100.0 * (maxLength - distance) / maxLength;
+        return maxLength == 0 ? 100.0 : 100.0 * (maxLength - distance) / maxLength;
+    }
+
+    public static double gcContent(String sequence) {
+        int gcCount = 0;
+        for (char c : sequence.toCharArray()) {
+            if (c == 'G' || c == 'C') gcCount++;
+        }
+        return 100.0 * gcCount / sequence.length();
     }
 }
